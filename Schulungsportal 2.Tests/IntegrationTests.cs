2b1d0392ece141de596d1691febbc4fd3ee38cd7
oliver <o-lohr@gmx.de>
2019-07-24 19:21:11 +0200

    Invalid range tests

--- Schulungsportal 2.Tests/IntegrationTests.cs
+++ Schulungsportal 2.Tests/IntegrationTests.cs
@@ -37,5 +37,22 @@ namespace Schulungsportal_2_Tests
             var expectedJson = "[{\"schulungGUID\":\"00000000-0000-0000-0000-000000000001\",\"titel\":\"Schulung 2\",\"organisatorInstitution\":\"CC\",\"beschreibung\":\"Schulung 2\",\"ort\":\"Büro\",\"anmeldefrist\":\"2019-06-18T00:00:00\",\"termine\":[{\"start\":\"2019-06-21T00:00:00\",\"end\":\"2019-06-22T00:00:00\"}],\"anmeldungsZahl\":0,\"isAbgesagt\":false},{\"schulungGUID\":\"00000000-0000-0000-0000-000000000000\",\"titel\":\"Schulung 1\",\"organisatorInstitution\":\"CC\",\"beschreibung\":\"Schulung 1\",\"ort\":\"Büro\",\"anmeldefrist\":\"2019-06-20T00:00:00\",\"termine\":[{\"start\":\"2019-06-21T00:00:00\",\"end\":\"2019-06-22T00:00:00\"}],\"anmeldungsZahl\":4,\"isAbgesagt\":false}]";
             Assert.Equal(expectedJson, gotJson);
         }
+
+        [Theory]
+        // too low
+        [InlineData(-1,5)]
+        [InlineData(0,-5)]
+        // too high
+        [InlineData(0,201)]
+        public async Task Get_SchulungsApiAllInvalidRange(int offset, int max) {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/api/schulungen?offset="+offset+"&max="+max);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
     }
 }
