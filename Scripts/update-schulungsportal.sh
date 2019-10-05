d3d0b33de05f908349838b58c809cd1fc5bc2f5e
oliver <o-lohr@gmx.de>
2019-10-05 17:26:40 +0200

    fix to actually copy new files, fixed link

--- Scripts/update-schulungsportal.sh
+++ Scripts/update-schulungsportal.sh
@@ -1,4 +1,4 @@
 # this comes after the new version got copied to /tmp/schulungsportal2
-cp -r /tmp/schulungsportal /var/www/schulungsportal2/publish/
+cp -rf /tmp/schulungsportal/* /var/www/schulungsportal2/publish/
 # this specific sudo command is allowed in the sudoers file, restart the service
 sudo /usr/sbin/service dotnet-schulungsportal restart
