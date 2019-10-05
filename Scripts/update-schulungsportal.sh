7bcdbd727058ec25df0ca7cd21140be04a98ea13
oliver <o-lohr@gmx.de>
2019-10-05 16:16:43 +0200

    should work now, deploy is now master only

--- Scripts/update-schulungsportal.sh
+++ Scripts/update-schulungsportal.sh
@@ -1,4 +1,4 @@
 # this comes after the new version got copied to /tmp/schulungsportal2
 cp -r /tmp/schulungsportal2 /var/www/schulungsportal2/publish/
 # this specific sudo command is allowed in the sudoers file, restart the service
-sudo service dotnet-schulungsportal restart
+sudo /usr/sbin/service dotnet-schulungsportal restart
